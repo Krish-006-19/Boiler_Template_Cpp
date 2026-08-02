eb7aca96937a2ba7242f2b3c364ee691bc309605
Krish Ramena <krish.ramena24@vit.edu>
2026-08-02 10:44:29 +0530

    Create Boiler_Template.cpp

---
+++ Boiler_Template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+#define fo(i,n) for(ll i=0;i<n;i++)
+#define of(i,n) for(ll i=n-1;i>=0;i--)
+#define forn(i,n) for(ll i=0;i<n-1;i++)
+#define forp(i,n) for(ll i=1;i<n;i++)
+#define fornp(i,n) for(ll i=1;i<n-1;i++)
+#define read(a) for(auto &x:a) cin>>x
+void solve();
+void fastio() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+}
+int main() {
+	fastio();
+	ll t;
+	cin >> t;
+
+	while(t--) solve();
+}
+
+void solve() {
+    
+}
